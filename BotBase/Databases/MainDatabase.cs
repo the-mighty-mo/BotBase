f27450231cd4318a22752f26cb6f473c927c8e25
Benjamin Hall <hallb1016@gmail.com>
2020-10-11 17:35:01 -0400

    Renamed cnDatabase to connection to be more universal

--- BotBase/Databases/MainDatabase.cs
+++ BotBase/Databases/MainDatabase.cs
@@ -6,21 +6,21 @@ namespace BotBase.Databases
 {
     public class MainDatabase
     {
-        private readonly SqliteConnection cnDatabase = new SqliteConnection("Filename=Database.db");
+        private readonly SqliteConnection connection = new SqliteConnection("Filename=Database.db");
 
         public DatabaseTable Database;
 
         public MainDatabase()
         {
-            Database = new DatabaseTable(cnDatabase);
+            Database = new DatabaseTable(connection);
         }
 
         public async Task InitAsync()
         {
-            await cnDatabase.OpenAsync();
+            await connection.OpenAsync();
 
             List<Task> cmds = new List<Task>();
-            using (SqliteCommand cmd = new SqliteCommand("CREATE TABLE IF NOT EXISTS Database (guild_id TEXT PRIMARY KEY, data TEXT NOT NULL);", cnDatabase))
+            using (SqliteCommand cmd = new SqliteCommand("CREATE TABLE IF NOT EXISTS Database (guild_id TEXT PRIMARY KEY, data TEXT NOT NULL);", connection))
             {
                 cmds.Add(cmd.ExecuteNonQueryAsync());
             }
@@ -28,13 +28,13 @@ namespace BotBase.Databases
             await Task.WhenAll(cmds);
         }
 
-        public async Task CloseAsync() => await cnDatabase.CloseAsync();
+        public async Task CloseAsync() => await connection.CloseAsync();
 
         public class DatabaseTable
         {
-            private readonly SqliteConnection cnDatabase;
+            private readonly SqliteConnection connection;
 
-            public DatabaseTable(SqliteConnection cnDatabase) => this.cnDatabase = cnDatabase;
+            public DatabaseTable(SqliteConnection connection) => this.connection = connection;
         }
     }
 }
